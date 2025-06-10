f783711fefb978c5c9f8d84e882ea7dae8406a76
pradip2296 <nagarepradip2005@gmail.com>
2025-06-10 09:59:29 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,15 @@
+class A
+{
+ public   int a;
+          int b;
+}
+class B
+{
+    public static void main(String args[])
+    {
+        A a1=new A();
+        a1.a=54;
+        a1.b=56;
+        System.out.println(a1.a+a1.b);
+    }
+}
